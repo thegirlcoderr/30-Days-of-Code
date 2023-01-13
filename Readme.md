1135fad5e89ef97bc4a143cb774e8de8aa0b54bc
thegirlcoderr <lynndy2015@gmail.com>
2023-01-13 19:28:22 +0100

    updated purchase button

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# 30-Days-of-Code
+ 
+In this challenge, I will be submitting little to medium size projects. I'm doing this to boost my HTML, CSS & JS skills. Down here you will find a couple of live previews and breakdown of what I learned.
---
+++ day5/index.js
@@ -0,0 +1,13 @@
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
+
+let Error = document.getElementById("error")
+
+
+
+function purchase() {
+   
+    Error.textcontent= "Opps"+ "Something went wrong, please try again"
+}
+ purchase()
