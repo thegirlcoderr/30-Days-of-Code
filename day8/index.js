9e0121b3e61db43f0b5297a56af86affd754c556
thegirlcoderr <lynndy2015@gmail.com>
2023-01-18 20:13:30 +0100

    initial commit

---
+++ day8/index.js
@@ -0,0 +1,30 @@
+let displayscoreHome = document.getElementById("displayscore-home")
+homescore = 0
+let displayscoreGuest = document.getElementById("displayscore-guest")
+
+function addhomescoreone() {
+    homescore = homescore + 1
+ displayscoreHOME.textcontent= homescore 
+}
+function addhomescorettwo(){
+  homescore = homescore + 2
+ displayscoreHOME.textcontent= homescore 
+}
+function addhomescorethree(){
+  homescore = homescore + 3
+ displayscoreHOME.textcontent= homescore 
+}
+
+
+function addguestscore1() {
+    homescore = homescore + 1
+ displayscoreHOME.textcontent= homescore 
+}
+function addguestscore2(){
+  homescore = homescore + 2
+ displayscoreHOME.textcontent= homescore 
+}
+function addguestscore3() {
+    homescore = homescore + 3
+    displayscoreHOME.textcontent = homescore
+}
