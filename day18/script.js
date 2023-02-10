d3e1fd83f8f0f2d0b1c26cc64b88d836b1fc8266
thegirlcoderr <lynndy2015@gmail.com>
2023-02-10 19:46:42 +0100

    added event listener and scroll function

--- day18/script.js
+++ day18/script.js
@@ -4,12 +4,26 @@ const menu = document.getElementById('mobile-menu');
 const counters = document.querySelectorAll('.counter');
 
 btn.addEventListener('click', navToggle);
+document.addEventListener('scroll', scrollPage);
+
 
 function navToggle() {
     btn.classList.toggle('open')  
     overlay.classList.toggle('overlay-show')
     document.body.classList.toggle('stop-scrolling')
     menu.classList.toggle('show-menu')
+}
+
+function scrollPage() {
+  const scrollPos = window.scrollY;
+
+  if (scrollPos > 100 && !scrollStarted) {
+    countUp();
+    scrollStarted = true;
+  } else if (scrollPos < 100 && scrollStarted) {
+    reset();
+    scrollStarted = false;
+  }
 }
 
 function countUp() {
