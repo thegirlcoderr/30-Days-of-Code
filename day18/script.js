276f0f5b33434e1efbe691dda698f434fc999fd5
thegirlcoderr <lynndy2015@gmail.com>
2023-02-10 16:49:36 +0100

    added functions

--- day18/script.js
+++ day18/script.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
 const menu = document.getElementById('mobile-menu');
+const counters = document.querySelectorAll('.counter');
 
 btn.addEventListener('click', navToggle);
 
@@ -10,3 +11,37 @@ function navToggle() {
     document.body.classList.toggle('stop-scrolling')
     menu.classList.toggle('show-menu')
 }
+
+function countUp() {
+    counters.forEach((counter) => {
+        counter.innerText = '0';
+
+        const updateCounter = () => {
+            //This will get count target
+            const target = +counter.getAttribute('data-target');
+           // This will help get the current target
+            const c = +counter.innerText;
+
+            //To create an increment
+            const increment = target / 100;
+
+            // If counter is less than target, add increment
+            if (c < target) {
+                //Round up and set counter value
+                counter.innerText = `${Math.ceil(c + increment)}`;
+
+
+                setTimeout(updateCounter, 75);
+            
+                
+            } else {
+                counter.innerText = target;
+        
+            }
+        };
+
+        updateCounter();
+
+   }) 
+}
+countUp()
